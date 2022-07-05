b52d90f15cc58354b4ff0c44eecfcd4f2a0b5f3c
Arun Kamble <arun.kamble@hurix.com>
2022-07-05 18:24:51 +0530

    pushed for testing purpose

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Header from './MyComponents/Header';
+import Counter from './features/counter/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/counter/Counter.js
@@ -0,0 +1,16 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./counterSlice";
+const Counter = () => {
+    const count = useSelector((state) => state.counter.count)
+    const dispatch = useDispatch();
+    console.log('count', count);
+  return (
+    <section>
+        <p>{count}</p>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+    </section>
+  )
+}
+
+export default Counter
